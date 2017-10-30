fdc8b42c297a07b8e906a21f72811b6ea4b64737
Unknown <freeplay@gmail.com>
2017-10-30 14:34:11 -0600

    adding install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo mkdir /usr/src/mk_arcade_joystick_rpi-0.1.5.5
+sudo cp -a * /usr/src/mk_arcade_joystick_rpi-0.1.5.5/
+sudo apt-get install -y --force-yes dkms cpp-4.7 gcc-4.7 joystick raspberrypi-kernel-headers wiringpi
+sudo dkms build -m mk_arcade_joystick_rpi -v 0.1.5.5
+sudo dkms install -m mk_arcade_joystick_rpi -v 0.1.5.5
+sudo sh -c 'echo "mk_arcade_joystick_rpi" >> /etc/modules'
+sudo sh -c 'echo "options mk_arcade_joystick_rpi map=4 gpio=4,17,6,5,19,26,16,24,23,18,15,14,-20" >> /etc/modprobe.d/mk_arcade_joystick.conf'

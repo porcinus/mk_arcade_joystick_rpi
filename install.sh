8a9c534e70df7a601c29cf24e66e4b8d03e675d5
porcinus <37425709+porcinus@users.noreply.github.com>
2019-04-19 22:27:22 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 sudo modprobe -r mk_arcade_joystick_rpi
+sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.5 --all
+sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.6 --all
+sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.7 --all
+sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.8 --all
+sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.9 --all
+sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.10 --all
+sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.11 --all
 sudo dkms remove -m mk_arcade_joystick_rpi -v 0.1.5.12 --all
 sudo mkdir /usr/src/mk_arcade_joystick_rpi-0.1.5.12
 sudo cp -a * /usr/src/mk_arcade_joystick_rpi-0.1.5.12/
